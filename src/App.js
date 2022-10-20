02dfd1b9932ede28015afb879a1908f40cbcca05
karume <karumekate8@gmail.com>
2022-10-20 23:36:37 +0300

    setting up components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import {
     Route,
 } from 'react-router-dom';
 import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
 
 const App = () => {
   return (
     <Routes>
           <Route exact path="/" element = { <Home />}/>
+          <Route  path="/about" element = { <About />}/>
+          <Route  path="/contact" element = { <Contact />}/>
           
     </Routes>
   )
---
+++ src/components/TaskForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Form } from 'react-router-dom'
+
+const TaskForm = () => {
+  return (
+    <div>
+        <form>
+        
+        </form>
+    </div>
+  )
+}
+
+export default TaskForm
